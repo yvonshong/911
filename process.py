c8271ba0772f44292185e1bb6fa42d9aa674a046
daiwei song <songdaiwei@hesaitech.com>
2023-05-16 20:49:10 +0800

    test612

---
+++ process.py
@@ -0,0 +1,24 @@
+import json
+
+if __name__ == "__main__":
+
+    f_txt = open("/home/songdaiwei/Documents/911/txt.txt")               # 返回一个文件对象   
+    line = f_txt.readline()
+    block_str = ""
+
+    block_id = 0
+    cnt = 0
+    while line:   
+        cnt += 1
+        # if cnt > 20:
+        #     break
+        line = f_txt.readline() 
+        print(line)  
+        block_str = line
+        block_dict = {}
+        block_dict["data"] = [block_str]
+        with open(f"{block_id}.json",'w',encoding='utf-8') as f_block:
+            json.dump(block_dict, f_block, ensure_ascii=False)
+        block_id+=1
+    
+    f_txt.close() 
